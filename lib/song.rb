902e38e3517ad5e6b56d20b56c3abfcc93e7eba6
Andre Skeene <halodre24@gmail.com>
2020-02-28 00:48:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,10 +2,13 @@ class Song
   attr_accessor :name, :artist, :genre
   @@count = 0
   @@artists = []
+  @@genres = []
 
   def initialize(song_title, artist_name, song_genre)
     @@count += 1
     @@artists << artist_name
+    @@genres << song_genre
+    
     @name = song_title
     @artist = artist_name
     @genre = song_genre
